3cd7421c585056b60a2cda4f5f28e62d56620d64
kucing31 <92510008+kucing31@users.noreply.github.com>
2021-10-15 08:53:25 +0700

    Add Modul0

---
+++ src/Modul0/InputKeyboard.java
@@ -0,0 +1,19 @@
+package Modul0;
+import java.util.Scanner;
+
+public class InputKeyboard {
+    public static void main(String[] args) {
+        Scanner masukkan = new Scanner(System.in);
+        int var_a;
+        int var_b;
+        System.out.println("Masukkan nilai var_a : ");
+        var_a = masukkan.nextInt();
+        System.out.println("Masukkan nilai var_b : ");
+        var_b = masukkan.nextInt();
+        System.out.println("Variabel yang terdapat dalam program");
+        System.out.println("var_a : " + var_a);
+        System.out.println("var_b : " + var_b);
+    }
+
+
+}
---
+++ src/Modul0/tipe.java
@@ -0,0 +1,23 @@
+package Modul0;
+
+public class tipe {
+    public static void main(String[] args) {
+        long data1    = 767226531;
+        int data2     = 2235641;
+        short data3   = 714;
+        byte data4    = 34;
+        double data5  = 4.967;
+        float data6   = (float) 1.733;
+        char data7    = 'C';
+        boolean data8 = true;
+
+        System.out.println("Nilai Long \t\t: " + data1);
+        System.out.println("Nilai Int \t\t: " + data2);
+        System.out.println("Nilai Short \t\t: " + data3);
+        System.out.println("Nilai Byte \t\t: " + data4);
+        System.out.println("Nilai Double \t\t: " + data5);
+        System.out.println("Nilai Float \t\t: " + data6);
+        System.out.println("Nilai Char \t\t: " + data7);
+        System.out.println("Nilai Boolean \t\t: " + data8);
+    }
+}
