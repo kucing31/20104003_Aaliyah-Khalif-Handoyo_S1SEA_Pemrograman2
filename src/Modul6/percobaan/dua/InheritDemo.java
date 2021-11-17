a7eec9042758c5752c9673847b681f917fc3eb7d
kucing31 <92510008+kucing31@users.noreply.github.com>
2021-11-17 17:32:20 +0700

    update

--- src/Modul6/percobaan/dua/InheritDemo.java
+++ src/Modul6/percobaan/dua/InheritDemo.java
@@ -3,5 +3,6 @@ package Modul6.percobaan.dua;
 public class InheritDemo {
     public static void main(String[] args) {
         Student Wahyu = new Student();
+        Person wahyu = new Person();
     }
 }
